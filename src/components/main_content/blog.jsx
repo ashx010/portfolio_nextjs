fbdf811198aae8dfcde438f12ec759f3a0c55a86
yash <yashaggarwal960@gmail.com>
2025-03-23 01:19:06 +0530

    update blog section, added sorting by popularity and date(i.e. original array)

--- src/components/main_content/blog.jsx
+++ src/components/main_content/blog.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+"use client";
+import React, {useState} from 'react';
 import {KanitFont} from "@/app/fonts";
 import Image from "next/image";
 
@@ -9,40 +10,59 @@ function Blog(props) {
             link: "https://medium.com/@scripet/2763cb4ec8d0",
             imgLink: "/post1.jpg",
             date: "Sep 20,2024",
-            type: "Web Development"
+            type: "Web Development",
+            popular: 4
         },
         {
             name: "Vs Code Secrets: Pro Developers’ TOP Shortcuts and Extensions Revealed!",
             link: "https://medium.com/@scripet/2d6099dd3478",
             imgLink: "/post2.jpg",
             date: "Sep 14,2024",
-            type: "Productivity"
+            type: "Productivity",
+            popular: 3
         },
         {
             name: "HOW TO set up Authentication in Next.js 14+ with Auth.js: A Step-by-Step Guide.",
             link: "https://medium.com/@scripet/b746203481d7",
             imgLink: "/post3.jpg",
             date: "Sep 7,2024",
-            type: "Web Development"
+            type: "Web Development",
+            popular: 1
         },
         {
             name: "HOW TO deploy Django project using Nginx and Gunicorn: A Step-by-Step Guide.",
             link: "https://medium.com/@scripet/69eee0f830d0",
             imgLink: "/post4.jpg",
             date: "Sep 2,2024",
-            type: "Backend Development"
+            type: "Backend Development",
+            popular: 2
         },
     ]
+    const [blogType, setBlogType] = useState("date")
+    const sortedBlogData = [...data].sort((a, b) => {
+        if (blogType === "popular") {
+            return a.popular - b.popular;
+        }
+        return 0;
+    });
+    const handleTogglePort = (type) => {
+        setBlogType(type);
+    }
+
     return (
         <div id="portfolio" className={`w-full rounded-[15px] bg-[#1C1E20FF] px-4 py-6 lg:px-10 lg:py-12 space-y-4`}>
             <h3 className={`font-mono font-medium uppercase text-lg tracking-wider relative pt-4 mb-5 dark:text-white before:content-['#'] before:pr-2 after:content-[attr(data-backdrop-text)] after:absolute after:top-0 after:left-0 after:font-poppins after:font-bold after:uppercase after:text-4xl after:opacity-15`} data-backdrop-text={"Blog"}>
                 Blog
             </h3>
             <h1 className={`text-2xl lg:text-3xl ${KanitFont.variable} font-[kanit] font-black tracking-wider`}>
-                Latest Blog Posts
+                Latest Medium Posts
             </h1>
+            <div className={`pt-2 lg:pt-6 flex lg:space-x-4 lg:space-y-0 space-y-4 lg:flex-row flex-col w-fit justify-start lg:items-center`}>
+                <div onClick={() => handleTogglePort("date")} className={`cursor-pointer border border-dashed rounded-xl px-3 py-1  tracking-wider font-mono ${blogType === "date" ? "text-white/90 border-white/90" : "text-white/40 border-white/40"} hover:text-white/80`}>Date</div>
+                <div onClick={() => handleTogglePort("popular")} className={`cursor-pointer border border-dashed rounded-xl  px-3 py-1  tracking-wider font-mono ${blogType === "popular" ? "text-white/90 border-white/90" : "text-white/40 border-white/40"} hover:text-white/80`}>Popular</div>
+            </div>
             <div className={`w-full pt-3 lg:pt-6 space-y-6 flex flex-col justify-start items-start`}>
-                {data
+                {sortedBlogData
                     .slice(0, 4)
                     .map((post, index) => (
                     <div key={index} className={`w-full flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-6 justify-start content-center`}>
